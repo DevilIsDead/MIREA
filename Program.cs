a0ab7559fcf5bf415dcaec29c8593c5dc76821ce
Izu <kash.izu333@gmail.com>
2023-07-30 22:16:07 +0300

    Start

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Configuration;
+using System.IO.Compression;
+using System.IO;
+using System.Net.FtpClient;
+using System.Net;
+
+class Program {
+
+    static void Main() {
+
+        string startPath;
+        var Path = ConfigurationManager.AppSettings["path"];
+        var ftpUrl = ConfigurationManager.AppSettings["ftpUrl"];
+        var ftpLogin = ConfigurationManager.AppSettings["ftpLogin"];
+        var ftpPassword = ConfigurationManager.AppSettings["ftpPassword"];
+        if (Path != null) {
+            startPath = (Path).ToString();
+            string Result = "zip_PC_" + ConfigurationManager.AppSettings["pcNum"];
+            if (File.Exists(Result)) {
+                File.Delete(Result);
+            }
+            ZipFile.CreateFromDirectory(startPath, Result);
+
+            if (ftpUrl != null && ftpLogin != null && ftpPassword != null) {
+                using (FtpClient client = new FtpClient()) {
+                        client.Host = ConfigurationManager.AppSettings["ftpUrl"];
+                        client.Credentials = new NetworkCredential(ConfigurationManager.AppSettings["ftpLogin"], ConfigurationManager.AppSettings["ftpPassword"]);
+
+                        using (Stream ostream = client.OpenWrite(Result)) {
+                            try {
+                                // istream.Position is incremented accordingly to the writes you perform
+                            }
+                            finally {
+                                ostream.Close();
+                            }
+                        }
+                }
+            } else {
+                Console.WriteLine("Wrong FTP information!");
+            }
+        } else {
+            Console.WriteLine("Wrong directory!");
+        }
+        Console.WriteLine("Press any key to continue.................");
+        Console.ReadKey();
+    }
+}
